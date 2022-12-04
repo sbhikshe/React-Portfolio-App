9c1a8af04a6202543260d27ef2daf823a57b14b4
Priya B <priya_saran@yahoo.com>
2022-12-04 07:02:39 -0800

    Used react-bootstrap Form component to add a contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,11 +1,25 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export default function Contact() {
   return (
-    <div>
-      <h2>
-        Contact
-      </h2>
-    </div>
+    <Form>
+      <Form.Group className="mb-3" id="name">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="string" placeholder="Enter your name"/>
+      </Form.Group>
+      <Form.Group className="mb-3" id="email">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Enter a valid email address"/>
+      </Form.Group>
+      <Form.Group className="mb-3" id="message">
+        <Form.Label>Message</Form.Label>
+        <Form.Control type="text" placeholder="Your message goes here ..."/>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
