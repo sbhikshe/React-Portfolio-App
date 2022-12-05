6aef2b82f2aedbf54f154c71aa0c590617412c6f
Priya B <priya_saran@yahoo.com>
2022-12-04 18:13:28 -0800

    Used Bootstrap React Form component for the Contact component, and css

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,19 +1,25 @@
 import React from "react";
+import '../css/Contact.css';
+
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 export default function Contact() {
+  const styles = {
+    "background": "#F2F5EA"
+  }
   return (
-    <Form>
-      <Form.Group className="mb-3" id="name">
+    <div className="form">
+    <Form className="p-5" style={styles}>
+      <Form.Group className="w-75 mb-3" id="name">
         <Form.Label>Name</Form.Label>
         <Form.Control type="string" placeholder="Enter your name"/>
       </Form.Group>
-      <Form.Group className="mb-3" id="email">
+      <Form.Group className="w-75 mb-3" id="email">
         <Form.Label>Email</Form.Label>
         <Form.Control type="email" placeholder="Enter a valid email address"/>
       </Form.Group>
-      <Form.Group className="mb-3" id="message">
+      <Form.Group className="w-75 mb-3" id="message">
         <Form.Label>Message</Form.Label>
         <Form.Control type="text" placeholder="Your message goes here ..."/>
       </Form.Group>
@@ -21,5 +27,6 @@ export default function Contact() {
         Submit
       </Button>
     </Form>
+    </div>
   );
 }
