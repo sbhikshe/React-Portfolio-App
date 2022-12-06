6d5daeaaf45cd32bccbf25a8b9cc5f6e7a514280
Priya B <priya_saran@yahoo.com>
2022-12-05 18:40:34 -0800

    Display active page link in navbar in a different color

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,10 +5,24 @@ export default function Navigation({content, handleContentChange}) {
 
   return (
     <nav className='navigation'>
-        <a href="#about" onClick={() => handleContentChange('About')}>About</a>
-        <a href="#portfolio" onClick={() => handleContentChange('Portfolio')}>Portfolio</a>
-        <a href="#resume" onClick={() => handleContentChange('Resume')}>Resume</a>
-        <a href="#contact" onClick={() => handleContentChange('Contact')}>Contact</a>
+        <a href="#about" 
+          onClick={() => handleContentChange('About')}
+          className={content === 'About' ? 'nav-link active' : 'nav-link'}>
+          About
+        </a>
+        <a href="#portfolio" 
+          onClick={() => handleContentChange('Portfolio')}
+          className={content === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+          Portfolio
+        </a>
+        <a href="#resume" onClick={() => handleContentChange('Resume')}
+          className={content === 'Resume' ? 'nav-link active' : 'nav-link'}>
+          Resume
+        </a>
+        <a href="#contact" onClick={() => handleContentChange('Contact')}
+          className={content === 'Contact' ? 'nav-link active' : 'nav-link'}>
+          Contact
+        </a>
     </nav>
   );
 }
