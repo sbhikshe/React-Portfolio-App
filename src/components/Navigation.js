0d4d0a6e7258462449f03d07729f1889e9a1a214
Priya B <priya_saran@yahoo.com>
2022-12-05 19:09:00 -0800

    Fixes to the Portfolio page to show background image and links to application and github

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,25 +4,33 @@ import './css/Navigation.css';
 export default function Navigation({content, handleContentChange}) {
 
   return (
-    <nav className='navigation'>
+    <nav className='navigation nav'>
+        <li className="nav-item">
         <a href="#about" 
           onClick={() => handleContentChange('About')}
           className={content === 'About' ? 'nav-link active' : 'nav-link'}>
           About
         </a>
+        </li>
+        <li className="nav-item">
         <a href="#portfolio" 
           onClick={() => handleContentChange('Portfolio')}
           className={content === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
           Portfolio
         </a>
+        </li>
+        <li className="nav-item">
         <a href="#resume" onClick={() => handleContentChange('Resume')}
           className={content === 'Resume' ? 'nav-link active' : 'nav-link'}>
           Resume
         </a>
+        </li>
+        <li className="nav-item">
         <a href="#contact" onClick={() => handleContentChange('Contact')}
           className={content === 'Contact' ? 'nav-link active' : 'nav-link'}>
           Contact
         </a>
+        </li>
     </nav>
   );
 }
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../css/Project.css';
 
 /* Add Github icon from FontAwesomeIcon React component */
@@ -9,15 +9,57 @@ library.add(fab);
 
 export default function Project({title, imageUrl, ghLink, appLink}) {
 
-  return (
-    <div className="project">
+  /* for the background image style: default is opaque.
+    onMouseOver, links are visible and image is transparent,
+    else (onMouseLeave), links are invisible and image is opaque */
+  const styleOpaque = {
+    backgroundImage: `url(${imageUrl})`,
+    backgroundSize: "cover",
+    backgroundRepeat:"no-repeat",
+    //"opacity": "1"
+    backgroundColor: "currentcolor"
+  }
+
+  const styleTransparent = {
+    backgroundImage: `url(${imageUrl})`,
+    backgroundSize: "cover",
+    backgroundRepeat:"no-repeat",
+    //"opacity": "0.2"
+    backgroundColor: "transparent"
+  }
+
+  /* app link and github link are visible when isLinkVisible is true */
+  /* initial value should be false, and only image is visible */
+  const [isLinkVisible, setVisible] = useState(false);
+  const [imageStyle, setImageStyle] = useState(styleOpaque);
+  const [linkOpacity, setLinkOpacity] = useState(0);
+
+  const handleMouseOver = () => { 
+    setVisible(true);
+    setImageStyle(styleTransparent);
+    setLinkOpacity(1);
+  };
+  const handleMouseLeave = () => { 
+    setVisible(false);
+    setImageStyle(styleOpaque); 
+    setLinkOpacity(0);
+  };
+
+  /*
       <img className="img-fluid"
-            src={imageUrl} 
-            alt='projectImage'></img>
-      <a href={ghLink}>
-        <FontAwesomeIcon icon={['fab', 'github']} />
-      </a>
-      <a href={appLink}>{title}</a>
-    </div>
+      src={imageUrl} 
+      alt='projectImage' hidden={true}></img>
+  */
+  return (
+    <div className="project"  
+          style={imageStyle}
+          onMouseOver={handleMouseOver}
+          onMouseLeave={handleMouseLeave}
+          >
+                <a href={appLink} opacity={linkOpacity}>{title}</a>
+                <a href={ghLink} opacity={linkOpacity}>
+                  <FontAwesomeIcon icon={['fab', 'github']} size="2x"/>
+                </a>
+    </div> 
   );
 }
